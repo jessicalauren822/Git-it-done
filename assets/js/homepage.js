0c2541d825227e4561a9d558579568e481217f73
jessicawright822 <104212202+jessicawright822@users.noreply.github.com>
2022-05-30 17:07:34 -0400

    user repo able to be pulled by searching

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,4 +9,23 @@ var getUserRepos = function(user) {
         })
     })
 }
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert("Please enter a GitHub username");
+    }
+};
   
+userFormEl. addEventListener("submit", formSubmitHandler);
