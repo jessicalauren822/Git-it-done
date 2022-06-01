9ca2196e1aa26fb55ba4989457b4cc63ed22ccd1
jessicawright822 <104212202+jessicawright822@users.noreply.github.com>
2022-06-01 15:48:53 -0400

    user will not be taken to the different html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -4,24 +4,42 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    var getRepoName = function() {
+        // grab repo name from url query string
+        var queryString = document.location.search;
+        var repoName = queryString.split("=")[1];
+      
+        if (repoName) {
+          // display repo name on the page
+          repoNameEl.textContent = repoName;
+      
+          getRepoIssues(repoName);
+        } else {
+          // if no repo was given, redirect to the homepage
+          document.location.replace("./index.html");
+        }
+      };
+
+    //make a get request to url
     fetch(apiUrl).then(function(response) {
         //request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                //pass response data to dom function
                 displayIssues(data);
-            });
-        }
+            
             //check if api has paginated issues
             if (response.headers.get("Link")) {
                 displayWarning(repo);
             }
-
+            });
+        }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
-
+    
+getRepoName();
 };
 
 getRepoIssues("facebook/react");
